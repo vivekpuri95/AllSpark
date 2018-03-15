7cdbe818ce62cc312169e62d1bef4ddc3e101abe
Deepak <dipak@clicklabs.co>
2018-03-15 14:23:34 +0530

    Added checks

---
+++ server/www/user/roles.js
@@ -0,0 +1,43 @@
+const API = require('../../utils/api');
+
+exports.list = class extends API {
+    async list() {
+        console.log(this.user);
+        return await this.mysql.query('select * from tb_roles where account_id = ? ', [this.account.account_id]);
+    }
+}
+
+exports.insert = class extends API {
+    async insert() {
+        let params = {};
+        params.account_id = this.account.account_id;
+        params.name = this.request.query.name;
+        params.is_admin = this.request.query.is_admin;
+
+        return await this.mysql.query('insert into tb_roles set ?', [params], 'write');
+    }
+}
+
+exports.update = class extends API {
+    async update() {
+        let params = {};
+        params.account_id = this.account.account_id;
+        params.name = this.request.query.name;
+        params.is_admin = this.request.query.is_admin;
+
+        this.request.query.account_id = this.account.account_id;
+        return await this.mysql.query('update tb_roles set ? where role_id = ? and account_id = ?',
+            [params, role_id, this.account.account_id],
+            'write'
+        );
+    }
+}
+
+exports.delete = class extends API {
+    async delete() {
+        return await this.mysql.query('delete from tb_roles where role_id = ? and account_id = ?',
+            [this.request.query.role_id, this.account.account_id],
+            'write'
+        );
+    }
+}
