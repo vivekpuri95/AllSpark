08b7a3c4846b8deb2ec06951c7abd73bd86cd378
Deepak <dipak@clicklabs.co>
2018-03-15 16:34:28 +0530

    Refactor

--- server/www/user/roles.js
+++ server/www/user/roles.js
@@ -2,22 +2,22 @@ const API = require('../../utils/api');
 
 exports.list = class extends API {
     async list() {
-        this.user.privilege.has('administrator');
+        this.user.privilege.needs('administrator');
         return await this.mysql.query('SELECT * FROM tb_roles WHERE account_id = ? ', [this.account.account_id]);
     }
 }
 
 exports.insert = class extends API {
     async insert() {
-        this.user.privilege.has('administrator');
+        this.user.privilege.needs('administrator');
 
         if(!this.request.body.name || !this.request.body.is_admin)
             return false;
 
-        let params = {
-        account_id: this.account.account_id,
-        name: this.request.body.name,
-        is_admin: this.request.body.is_admin
+        const params = {
+            account_id: this.account.account_id,
+            name: this.request.body.name,
+            is_admin: this.request.body.is_admin
         };
 
         return await this.mysql.query('INSERT INTO tb_roles SET ?', [params], 'write');
@@ -26,16 +26,17 @@ exports.insert = class extends API {
 
 exports.update = class extends API {
     async update() {
-        this.user.privilege.has('administrator');
+        this.user.privilege.needs('administrator');
 
         let params = {
-        account_id: this.account.account_id,
-        name: this.request.body.name,
-        is_admin: this.request.body.is_admin
+            account_id: this.account.account_id,
+            name: this.request.body.name,
+            is_admin: this.request.body.is_admin
         };
 
         this.request.body.account_id = this.account.account_id;
-        return await this.mysql.query('UPDATE tb_roles SET ? WHERE role_id = ? AND account_id = ?',
+        return await this.mysql.query(
+            'UPDATE tb_roles SET ? WHERE role_id = ? AND account_id = ?',
             [params, this.request.body.role_id, this.account.account_id],
             'write'
         );
@@ -44,9 +45,10 @@ exports.update = class extends API {
 
 exports.delete = class extends API {
     async delete() {
-        this.user.privilege.has('administrator');
+        this.user.privilege.needs('administrator');
 
-        return await this.mysql.query('DELETE FROM tb_roles WHERE role_id = ? AND account_id = ?',
+        return await this.mysql.query(
+            'DELETE FROM tb_roles WHERE role_id = ? AND account_id = ?',
             [this.request.body.role_id, this.account.account_id],
             'write'
         );
