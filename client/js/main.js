c65d2fe82c4c1c4fdc46cf312d4da845a26f8fd0
achshar <achshar2@gmail.com>
2018-03-16 23:06:31 +0530

    Client now derives server's port

--- client/js/main.js
+++ client/js/main.js
@@ -20,7 +20,7 @@ class Account {
 
 	static async fetch() {
 
-		window.account = {APIHost: `https://${window.location.host}:${window.location.hostname == 'localhost' ? '3002' : '3000'}/`}
+		window.account = {APIHost: `https://${window.location.host}:${PORT - 5080}/`}
 
 		try {
 
@@ -38,7 +38,7 @@ class Account {
 		for(const key in account)
 			this[key] = account[key];
 
-		this.APIHost = `https://${this.url}:${window.location.hostname == 'localhost' ? '3002' : '3000'}/`;
+		this.APIHost = `https://${this.url}:${PORT - 5080}/`;
 	}
 }
 
