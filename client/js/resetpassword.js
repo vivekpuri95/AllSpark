27a7f7c1730c2d2bea256a1928aaff4b0df400b4
Deepak <dipak@clicklabs.co>
2018-03-23 16:17:15 +0530

    forgot endpoint change

--- client/js/resetpassword.js
+++ client/js/resetpassword.js
@@ -31,7 +31,7 @@ class ResetPassword extends Page {
 			method: 'POST',
 			form: new FormData(ResetPassword.form)
 		};
-		const response = await API.call('user/password/reset', {}, options);
+		const response = await API.call('authentication/reset', {}, options);
 
 		if(response) {
 
