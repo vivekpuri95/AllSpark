e5bd4c6fc992b67487b913827e3893ffc6d2b186
Deepak <dipak@clicklabs.co>
2018-04-06 16:45:54 +0530

    Added frontend for execution time. Closing #148

--- client/js/reports.js
+++ client/js/reports.js
@@ -605,9 +605,12 @@ class Report {
 
 			const response = await API.call('reports/engine/report', parameters, options) || [];
 
-			content.rowCount.innerHTML = `
-				Rows: <strong>${Format.number(response ? response.data.length : 0)}</strong>,
-				Columns: <strong>${Format.number(response ? Object.keys(response.data[0]).length : 0)}</strong>
+            content.rowCount.innerHTML = `
+				<span>Execution Time: <strong>${Format.number(response.execution_time / 1000)}s</strong></span>
+				<span>Cached: <strong>${response.redis.status ? 'Yes' : 'No'}</strong></span>
+				<span>Cache Age: <strong>${response.redis.status ? Format.number(response.redis.age / 1000 / 60 / 60) : '0'}h</strong></span>
+				<span>Rows: <strong>${Format.number(response ? response.data.length : 0)}</strong></span>
+				<span>Columns: <strong>${Format.number(response ? Object.keys(response.data[0]).length : 0)}</strong></span>
 			`;
 
 			content.json.innerHTML = `<code>${JSON.stringify(response.data, 0, 1)}</code>`;
