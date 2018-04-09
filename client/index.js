052552d3a283f4162833bc5e651b5f4b3d212763
Ravi kant <ravikant.kant5555@gmail.com>
2018-04-09 15:10:46 +0530

    routing

--- client/index.js
+++ client/index.js
@@ -809,56 +809,58 @@ app.get('/settings/:tab?/:id?', (req, res) => {
 			<a href="/settings/datasets">DataSets</a>
 		</nav>
 
-		<section class="section" id="list">
+		<div class="datasets-ui hidden">
+			<section class="section" id="list">
 
-			<h1>Datasets Manage</h1>
+				<h1>Datasets Manage</h1>
 
-			<header class="toolbar">
-				<button id="add-datset"><i class="fa fa-plus"></i> Add New Dataset</button>
-			</header>
+				<header class="toolbar">
+					<button id="add-datset"><i class="fa fa-plus"></i> Add New Dataset</button>
+				</header>
 
-			<table class="block">
-				<thead>
-					<tr>
-						<th>ID</th>
-						<th>Name</th>
-						<th>Category</th>
-						<th>Query id</th>
-						<th class="action">Edit</th>
-						<th class="action">Delete</th>
-					</tr>
-				</thead>
-				<tbody></tbody>
-			</table>
-		</section>
+				<table class="block">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Category</th>
+							<th>Query id</th>
+							<th class="action">Edit</th>
+							<th class="action">Delete</th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</section>
 
-		<section class="section" id="form">
+			<section class="section" id="form">
 
-			<h1></h1>
+				<h1></h1>
 
-			<header class="toolbar">
-				<button id="cancel-form"><i class="fa fa-arrow-left"></i> Back</button>
-				<button type="submit" form="user-form"><i class="fa fa-save"></i> Save</button>
-			</header>
+				<header class="toolbar">
+					<button id="cancel-form"><i class="fa fa-arrow-left"></i> Back</button>
+					<button type="submit" form="user-form"><i class="fa fa-save"></i> Save</button>
+				</header>
 
-			<form class="block form" id="user-form">
+				<form class="block form" id="user-form">
 
-				<label>
-					<span>Name</span>
-					<input type="text" name="name" required>
-				</label>
+					<label>
+						<span>Name</span>
+						<input type="text" name="name" required>
+					</label>
 
-				<label>
-					<span>Category</span>
-					<select name="category_id"></select>
-				</label>
+					<label>
+						<span>Category</span>
+						<select name="category_id"></select>
+					</label>
 
-				<label>
-					<span>Query Id</span>
-					<input type="number" name="query_id">
-				</label>
-			</form>
-		</section>
+					<label>
+						<span>Query Id</span>
+						<input type="number" name="query_id">
+					</label>
+				</form>
+			</section>
+		</div>	
 
 	`));
 });
--- client/js/settings.js
+++ client/js/settings.js
@@ -3,7 +3,14 @@ Page.class = class Settings extends Page {
 	constructor() {
 		super();
 		const loader = window.location.pathname.split('/');
-		let which_class = loader[loader.indexOf('settings')+1]
+		let which_class = loader[loader.indexOf('settings')+1];
+
+		Array.from(this.container.querySelectorAll('nav a')).map(a => {if(a.href == window.location.href){a.classList.add('selected')}})	
+
+		if(!which_class) {
+			window.location.href = this.container.querySelector('nav a').href;
+		}
+
 		new window[which_class](this);
 	}
 }
@@ -20,17 +27,18 @@ window.datasets = class Datasets extends SettingPage {
 
 	setup(container) {
 
-		Datasets.container = container;
-		Dataset.form_container = Datasets.container.querySelector('section#form');
-		Dataset.form = Dataset.form_container.querySelector('form');
+		Datasets.container = container.querySelector('.datasets-ui');
+		Datasets.container.classList.remove('hidden');
+		Setting_Dataset.form_container = Datasets.container.querySelector('section#form');
+		Setting_Dataset.form = Setting_Dataset.form_container.querySelector('form');
 
 		for(const data of MetaData.categories.values()) {
-			Dataset.form.category_id.insertAdjacentHTML('beforeend',`
+			Setting_Dataset.form.category_id.insertAdjacentHTML('beforeend',`
 				<option value="${data.category_id}">${data.name}</option>
 			`);
 		}
 
-		Datasets.container.querySelector('section#list #add-datset').on('click', () => Dataset.add());
+		Datasets.container.querySelector('section#list #add-datset').on('click', () => Setting_Dataset.add());
 
 		Datasets.container.querySelector('section#form #cancel-form').on('click', () => {
 			Sections.show('list');
@@ -44,7 +52,7 @@ window.datasets = class Datasets extends SettingPage {
 		Datasets.list = new Map;
 
 		for(const data of response)
-			Datasets.list.set(data.id, new Dataset(data, this));
+			Datasets.list.set(data.id, new Setting_Dataset(data, this));
 
 		Datasets.render();
 	}
@@ -61,7 +69,7 @@ window.datasets = class Datasets extends SettingPage {
 	}
 }
 
-class Dataset {
+class Setting_Dataset {
 
 	constructor(datset, datsets) {
 		for(const key in datset)
@@ -76,7 +84,7 @@ class Dataset {
 
 		const options = {
 			method: 'POST',
-			form: new FormData(Dataset.form),
+			form: new FormData(Setting_Dataset.form),
 		}
 
 		const response = await API.call('datasets/insert', {}, options);
@@ -88,16 +96,16 @@ class Dataset {
 
 	static add() {
 
-		Dataset.form_container.querySelector('h1').textContent = 'Add new Dataset';
-		Dataset.form.reset();
+		Setting_Dataset.form_container.querySelector('h1').textContent = 'Add new Setting_Dataset';
+		Setting_Dataset.form.reset();
 
-		Dataset.form.removeEventListener('submit', Dataset.submitListener);
+		Setting_Dataset.form.removeEventListener('submit', Setting_Dataset.submitListener);
 
-		Dataset.form.on('submit', Dataset.submitListener = e => Dataset.insert(e));
+		Setting_Dataset.form.on('submit', Setting_Dataset.submitListener = e => Setting_Dataset.insert(e));
 
 		Sections.show('form');
 
-		Dataset.form.focus();
+		Setting_Dataset.form.focus();
 	}
 
 	get row() {
@@ -125,15 +133,15 @@ class Dataset {
 
 	async edit() {
 
-		Dataset.form_container.querySelector('h1').textContent = 'Edit ' + this.name;
-		Dataset.form.name.value = this.name;
+		Setting_Dataset.form_container.querySelector('h1').textContent = 'Edit ' + this.name;
+		Setting_Dataset.form.name.value = this.name;
 
-		Array.from(Dataset.form.category_id.querySelectorAll('option')).map(o => {if(o.value == this.category_id) o.selected = true});
+		Array.from(Setting_Dataset.form.category_id.querySelectorAll('option')).map(o => {if(o.value == this.category_id) o.selected = true});
 
-		Dataset.form.query_id.value = this.query_id;
+		Setting_Dataset.form.query_id.value = this.query_id;
 
-		Dataset.form.removeEventListener('submit', Dataset.submitListener);
-		Dataset.form.on('submit', Dataset.submitListener = e => this.update(e));
+		Setting_Dataset.form.removeEventListener('submit', Setting_Dataset.submitListener);
+		Setting_Dataset.form.on('submit', Setting_Dataset.submitListener = e => this.update(e));
 
 		Sections.show('form');
 	}
@@ -148,7 +156,7 @@ class Dataset {
 
 		const options = {
 			method: 'POST',
-			form: new FormData(Dataset.form),
+			form: new FormData(Setting_Dataset.form),
 		}
 
 		await API.call('datasets/update', parameter, options);
