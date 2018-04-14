a9264788ec023a39518991ea5c28c874c0b70478
achshar <achshar2@gmail.com>
2018-04-14 19:11:19 +0530

    Fixed dataset timing

--- client/js/main.js
+++ client/js/main.js
@@ -4299,6 +4299,6 @@ Node.prototype.on = window.on = function(name, fn) {
 	this.addEventListener(name, fn);
 }
 
-MetaData.timeout = 0;// 5 * 60 * 60 * 1000;
-Dataset.timeout = 0;//5 * 60 * 60 * 1000;
+MetaData.timeout = 0; // 5 * 60 * 1000;
+Dataset.timeout = 30 * 1000; // 5 * 60 * 1000;
 Visualization.animationDuration = 750;
