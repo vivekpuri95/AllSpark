320d1a327674598e1c1bf2cab8a71fdd80a84c7b
achshar <achshar2@gmail.com>
2018-04-28 14:20:04 +0530

    Fixed spatial maps preview on report editor

--- client/index.js
+++ client/index.js
@@ -272,7 +272,7 @@ router.get('/:type(dashboard|report)/:id?', (request, response) => {
 						Edit
 					</button>
 				</label>
-				
+
 				<label>
 					<button id="export-dashboard" class="hidden">
 						<i class="fa fa-download"></i>
@@ -307,7 +307,7 @@ router.get('/dashboards/:id?', (request, response) => {
 					<i class="fa fa-plus"></i>
 					Add New Dashboard
 				</button>
-				
+
 				<button type="button" id="import-dashboard">
 					<i class="fa fa-upload"></i>
 					Import
@@ -377,8 +377,8 @@ router.get('/:type(reports|visualization)/:id?', (request, response) => {
 		'https://cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/ace.js',
 		'https://cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/ext-language_tools.js',
 
-		// 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA_9kKMQ_SDahk1mCM0934lTsItV0quysU" defer f="',
-		// 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js" defer f="',
+		'https://maps.googleapis.com/maps/api/js?key=AIzaSyA_9kKMQ_SDahk1mCM0934lTsItV0quysU" defer f="',
+		'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js" defer f="',
 
 		'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js',
 	]);
