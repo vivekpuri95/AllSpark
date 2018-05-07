2542a115cedaed2f7d39f1f9cfcd912978c38783
meedeepak <0000deepak0000@gmail.com>
2018-05-07 12:14:31 +0530

    limit changed

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+app.use(bodyParser.json({limit: '50mb', parameterLimit: 100000}));
+app.use(bodyParser.urlencoded({limit: '50mb', parameterLimit: 100000, extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
