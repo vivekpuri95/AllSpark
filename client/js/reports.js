c293c2cbec201c0a903bac561315e2595d829b29
achshar <achshar2@gmail.com>
2018-05-08 12:54:45 +0530

    Button rename

--- client/js/reports.js
+++ client/js/reports.js
@@ -595,7 +595,7 @@ ReportsManger.stages.set('configure-report', class ConfigureReport extends Repor
 		const small = this.page.stages.get('pick-report').switcher.querySelector('small');
 
 		if(this.report) {
-			small.textContent = this.report.name + ` (${this.report.query_id})`;
+			small.textContent = this.report.name + ` #${this.report.query_id}`;
 			this.edit();
 		}
 
@@ -841,7 +841,7 @@ ReportsManger.stages.set('define-report', class DefineReport extends ReportsMang
 		this.container.querySelector('#filter-form').classList.add('hidden');
 		this.container.querySelector('#filter-list').classList.remove('hidden');
 
-		this.page.stages.get('pick-report').switcher.querySelector('small').textContent = this.report.name + ` (${this.report.query_id})`;
+		this.page.stages.get('pick-report').switcher.querySelector('small').textContent = this.report.name + ` #${this.report.query_id}`;
 	}
 
 	async update(e) {
@@ -1486,7 +1486,7 @@ ReportsManger.stages.set('configure-visualization', class ConfigureVisualization
 
 		this.dashboards.load();
 
-		this.page.stages.get('pick-report').switcher.querySelector('small').textContent = this.report.name + ` (${this.report.query_id})`;
+		this.page.stages.get('pick-report').switcher.querySelector('small').textContent = this.report.name + ` #${this.report.query_id}`;
 	}
 
 	async insert(e) {
