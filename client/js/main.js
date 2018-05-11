c0af8fe3e3836812eb69f46451607f38bbc52b07
achshar <achshar2@gmail.com>
2018-05-11 15:10:46 +0530

    Final clean up

--- client/js/main.js
+++ client/js/main.js
@@ -279,13 +279,9 @@ class User {
 
 	static logout(next) {
 
-		const
-			access_token = localStorage.access_token || '',
-			parameters = new URLSearchParams();
+		const parameters = new URLSearchParams();
 
 		localStorage.clear();
-
-		// localStorage.access_token = access_token || '';
 
 		if(next)
 			parameters.set('continue', window.location.pathname + window.location.search);
