4aa76ff2a59f9ca806095ccb6722360f6f704e74
meedeepak <0000deepak0000@gmail.com>
2018-05-22 20:07:07 +0530

    toggle features backend

--- server/www/accounts/features.js
+++ server/www/accounts/features.js
@@ -1,13 +1,13 @@
 const API = require('../../utils/api.js');
 
 exports.list = class extends API {
-	async list(){
+	async list() {
 		return await this.mysql.query('SELECT * from tb_account_features');
 	}
 }
 
-exports.toggle = class extends API{
-	async toggle(){
+exports.toggle = class extends API {
+	async toggle() {
 		return await this.mysql.query(`
 			INSERT INTO tb_account_features(account_id, feature_id) 
 			SELECT ?,? FROM dual WHERE if(? IN (select feature_id from tb_features), 1, 0) 
