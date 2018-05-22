0e353ca71cf33477a131f38c0b179e2a34923bca
Durbar Dasgupta <durbar.dasgupta@jungleworks.co>
2018-05-22 11:08:29 +0530

    added bigquery support in reporter

--- server/onServerStart.js
+++ server/onServerStart.js
@@ -1,20 +1,20 @@
-const API = require('./utils/api');
 const mysql = require('./utils/mysql').MySQL;
 const bigquery = require('./www/bigquery').setup;
 
 
 async function loadAccounts() {
 
-    const accountList = await mysql.query('select * from tb_accounts where status = 1');
-    const accountObj = {};
+	const accountList = await mysql.query('select * from tb_accounts where status = 1');
+	const accountObj = {};
 
-    accountList.map(x => accountObj[x.url] = x);
+	accountList.map(x => accountObj[x.url] = x);
 
-    global.account = accountObj;
+	global.account = accountObj;
 }
 
 async function loadBigquery() {
-    // await bigquery();
+
+	await bigquery();
 }
 
 exports.loadAccounts = loadAccounts;
--- server/www/bigquery.js
+++ server/www/bigquery.js
@@ -1,156 +1,99 @@
-// const gcloud = require('google-cloud');
-// const redis = require('./redis');
-// const moment = require('moment');
-// const mysql = require('./mysql').MySQL;
-// const fs = require('fs');
-// const commonFun = require('./commonFunctions');
+const gcloud = require('google-cloud');
+const mysql = require('../utils/mysql').MySQL;
+const fs = require('fs');
+const commonFun = require('../utils/commonFunctions');
+const config = require("config");
+const path = require("path");
 
-// const gauth = function (project, file) {
+const gauth = function (project, file) {
 
-//     console.log('Initiating authentication with gcloud..');
+	console.log('Initiating authentication with gcloud..');
 
-//     return gcloud.bigquery({
-//         projectId: project,
-//         keyFilename: file
-//     });
+	return gcloud.bigquery({
+		projectId: project,
+		keyFilename: file
+	});
 
-// };
+};
 
-// class BigQuery {
+class BigQuery {
 
-//     static call(queryName, query, callback) {
+	static call(query, filters, account, file, project) {
 
-//         return new Promise(function (resolve, reject) {
+		return new Promise(function (resolve, reject) {
 
-//             let queryHash = require('crypto').createHash('md5').update(query).digest("hex"),
-//                 uniKey = `bqFresh#${queryName}#${queryHash}`,
-//                 getRedis = Promise.resolve();
+			const options = {
+				"query": query,
+				"queryParameters": filters,
+				"useLegacySql": false,
+				"parameterMode": "NAMED"
+			};
 
-//             if(queryName)
-//                 getRedis = BigQuery.redisGet(uniKey);
+			gauth(project, path.join(__dirname , "../../bigquery_files/" + account + "/" + file)).query(options, function (err, bqData) {
 
-//             getRedis.then(function(bqData) {
+				if (err) {
+					console.log("in error ", err);
+					return reject({error: err, query: query});
+				}
 
-//                 if(bqData && commonFun.isJson(bqData))
-//                     return BigQuery.resolve(JSON.parse(bqData), callback, resolve);
+				resolve(bqData);
 
-//                 gauth().query(query, function(err, bqData) {
+			});
+		});
+	}
 
-//                     if(err)
-//                         return BigQuery.reject(["bqcallnocache", err, query], callback, reject);
 
-//                     BigQuery.resolve(bqData, callback, resolve);
+	static async setup() {
+		const bigQueryConnections = await mysql.query(`
+            select
+                c.*,
+                a.account_id
+            from
+                tb_credentials c
+            join
+                tb_accounts a
+                using(account_id)
+            where
+                type = 'bigquery'
+                and c.status = 1
+                and a.status = 1
+            `);
 
-//                     if(queryName)
-//                         BigQuery.redisSet(uniKey, JSON.stringify(bqData));
-//                 });
-//             });
-//         });
-//     }
+		const path = config.get("bigquery_files_destination");
 
-//     static resolve(response, callback, resolve) {
+		for (const con of bigQueryConnections) {
 
-//         if (callback)
-//             callback(null, response);
+			if (!fs.existsSync(path + con.account_id)) {
 
-//         resolve(response)
-//     }
+				fs.mkdirSync(path + con.account_id);
+			}
 
-//     static reject(err, callback, reject) {
+			else {
 
-//         if (callback)
-//             callback(err);
+				try {
 
-//         reject(err)
-//     }
+					await commonFun.clearDirectory(path + con.account_id);
+				}
+				catch (e) {
 
-//     static redisGet(queryName) {
-//         return new Promise((resolve, reject) => {
+					console.log(new Error('could not clear directory', e));
+				}
+			}
+		}
 
-//             redis.get(queryName, function(err, result) {
+		for (const con of bigQueryConnections) {
 
-//                 if(err)
-//                     return reject();
+			try {
 
-//                 resolve(result);
-//             });
-//         });
-//     }
+				await fs.writeFileSync(path + con.account_id + '/' + con.id + ".json", con.file);
+			}
+			catch (e) {
 
-//     static redisSet(queryName, value) {
+				console.log(new Error('could not load bigquery file: ', con.id, 'for ', con.name));
+			}
+		}
+	}
+}
 
-//         const expire_time = parseInt(moment().endOf('day').format('X'));
-
-//         return new Promise((resolve, reject) => {
-
-//             redis.set(queryName, value, function(err) {
-
-//                 if(err)
-//                     return reject(err);
-
-//                 redis.expireat(queryName, expire_time, function(err) {
-
-//                     if(err)
-//                         return reject(err);
-
-//                     console.log('stored in redis: ' + queryName);
-//                     resolve();
-//                 });
-//             });
-//         });
-//     }
-
-//     static async setup() {
-//         const bigQueryConnections = await mysql.query(`
-//             select
-//                 c.*,
-//                 a.name
-//             from
-//                 tb_credentials c
-//             join
-//                 tb_accounts a
-//                 using(account_id)
-//             where
-//                 type = 'bigquery'
-//                 and c.status = 1
-//                 and a.status = 1
-//             `);
-
-//         const path = "../../bigquery_files/";
-
-//         for(const con of bigQueryConnections) {
-
-//             if (!fs.existsSync(path + con.name)) {
-
-//                 fs.mkdirSync(path + con.name);
-//             }
-
-//             else {
-
-//                 try{
-
-//                     await commonFun.clearDirectory(path + con.name);
-//                 }
-//                 catch(e) {
-
-//                     console.log(new Error('could not clear directory', e))
-//                 }
-//             }
-//         }
-
-//         for(const con of bigQueryConnections) {
-
-//             try {
-
-//                 await fs.writeFileSync(path + con.name+'/' + con.connection_name + ".json", con.file);
-//             }
-//             catch(e) {
-
-//                 console.log(new Error('could not load bigquery file: ', con.connection_name, 'for ', con.name))
-//             }
-//         }
-//     }
-// }
-
-// exports.setup = BigQuery.setup;
-// exports.BigQuery = BigQuery;
+exports.setup = BigQuery.setup;
+exports.BigQuery = BigQuery;
