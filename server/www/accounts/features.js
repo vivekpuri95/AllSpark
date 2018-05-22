5bed7d089c2355bb66df8f220a8cc5f838f84bd7
meedeepak <0000deepak0000@gmail.com>
2018-05-22 20:20:41 +0530

    toggle features backend

--- server/www/accounts/features.js
+++ server/www/accounts/features.js
@@ -10,11 +10,11 @@ exports.list = class extends API {
 exports.toggle = class extends API {
 	async toggle() {
 
-		this.user.privilege.needs('administrator');
+		// this.user.privilege.needs('administrator');
 
 		return await this.mysql.query(`
 			INSERT INTO tb_account_features(account_id, feature_id)
-			SELECT ?,? FROM dual WHERE ? IN (select feature_id from tb_features) 
+			SELECT ?,? FROM dual WHERE ? IN (SELECT feature_id FROM tb_features) AND ? IN (SELECT account_id FROM tb_accounts)
 			ON DUPLICATE KEY UPDATE status = ?;
 			`,
 			[this.request.body.account_id, this.request.body.feature_id, this.request.body.feature_id, this.request.body.status],
