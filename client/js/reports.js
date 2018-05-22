b31ad690d82b8e4f9266456b012d4e3b10d219e2
meedeepak <0000deepak0000@gmail.com>
2018-05-22 16:18:35 +0530

    na class added

--- client/js/reports.js
+++ client/js/reports.js
@@ -660,6 +660,7 @@ class DataSource {
 		description.textContent = null;
 
 		if (!this.description && !this.visualizations.selected.description) {
+			description.classList.remove('NA');
 			description.classList.add('NA');
 			description.innerHTML = 'No description found!';
 		}
