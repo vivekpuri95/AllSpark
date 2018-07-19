b1fefa0ef137f213203354728d900daee9da0c09
achshar <achshar2@gmail.com>
2018-07-19 15:43:35 +0530

    Drill down UI fixes

--- client/js/reports.js
+++ client/js/reports.js
@@ -460,7 +460,7 @@ class DataSource {
 				const title = [];
 
 				for(const p of source.drilldown.parameters)
-					title.push(`${p.value}: ${p.selectedValue}`);
+					title.push(`${source.drilldown.parent.filters.has(p.placeholder) ? source.drilldown.parent.filters.has(p.placeholder).name : p.placeholder}: ${p.selectedValue}`);
 
 				link.title = title.join('\n');
 
