fdc697e1341311c466e130dff9b71cfd908fa537
ravikant96 <ravikant.kant5555@gmail.com>
2018-08-24 12:57:33 +0530

    dsns

--- client/js/settings.js
+++ client/js/settings.js
@@ -1272,8 +1272,8 @@ class PrivilegeComponents {
 				method: "POST",
 			},
 			parameters = {
-				parent: this.multiSelect.value[0],
-				privilege_id: this.privilege.privilege_id,
+				parent: this.privilege.privilege_id,
+				privilege_id: this.multiSelect.value[0],
 			};
 
 		const result = await API.call('privileges_manager/insert', parameters, options);
