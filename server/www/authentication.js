e76c1c81c8ad487945e825657eec3d50cccb6071
Achin Sharma <achshar2@gmail.com>
2018-11-26 11:41:16 +0530

    Changed short lived token to one minute.

--- server/www/authentication.js
+++ server/www/authentication.js
@@ -296,7 +296,7 @@ exports.login = class extends API {
 
 		let session = {};
 
-		const expiryTime = Math.floor(Date.now() / 1000) + (parseInt(this.userDetails.ttl || 30) * 86400);
+		const expiryTime = Math.floor(Date.now() / 1000) + (parseInt(this.userDetails.ttl || 30) * 60);
 
 		try {
 			sessionLogs.request = {};
