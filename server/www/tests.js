6660b15126f53104b22f5a77f776090af79b3231
Achin Sharma <achshar2@gmail.com>
2018-12-27 17:13:59 +0530

    removed schema change function

--- server/www/tests.js
+++ server/www/tests.js
@@ -5,39 +5,6 @@ class Tests extends API {
 	async test() {
 		return 'a';
 	}
-
-	async changeFilterOffsetFormat() {
-
-		for(const filter of await this.mysql.query('SELECT * FROM tb_query_filters')) {
-
-			if(isNaN(parseInt(filter.offset)))
-				continue;
-
-			let type = 'day';
-
-			if(filter.type == 'datetime')
-				type = 'seconds';
-
-			else if(filter.type == 'month')
-				type = 'month';
-
-			else if(filter.type == 'year')
-				type = 'year';
-
-			filter.offset = [{
-				value: Math.abs(filter.offset),
-				unit: type,
-				direction: filter.offset > 0 ? 1 : -1,
-				snap: true,
-			}];
-
-			await this.mysql.query(
-				'UPDATE tb_query_filters SET offset = ? WHERE filter_id = ?',
-				[JSON.stringify(filter.offset), filter.filter_id],
-				'write'
-			);
-		}
-	}
 }
 
 exports.test = Tests;
