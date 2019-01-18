933450d6d23e4c6c1ced1f2520f03486d156184e
vivekpuri95 <vivekpuri1995@gmail.com>
2019-01-18 18:55:02 +0530

    Update startpm2.sh

--- startpm2.sh
+++ startpm2.sh
@@ -3,11 +3,11 @@ environment="$1"
 
 cd /apps/node-apps/allspark/
 cp config/sample.json config/$environment.json
-sed -i "s|8080|<3001>|g" config/$environment.json
+sed -i "s|8080|3001|g" config/$environment.json
 sed -i "s|mysql-host|mysql-local|g" config/$environment.json
 sed -i "s|mysql-read|root|g" config/$environment.json
 sed -i "s|mysql-write|root|g" config/$environment.json
-sed -i "s|mysql-pass|<password>|g" config/$environment.json
+sed -i "s|mysql-pass|password|g" config/$environment.json
 sed -i "s|env-db|"$environment"_allspark|g" config/$environment.json
 NODE_ENV="$environment" pm2 start bin/www --name "$environment"
 
@@ -17,7 +17,7 @@ sed -i "s|8080|8001|g" config/$environment.json
 sed -i "s|mysql-host|mysql-local|g" config/$environment.json
 sed -i "s|mysql-read|root|g" config/$environment.json
 sed -i "s|mysql-write|root|g" config/$environment.json
-sed -i "s|mysql-pass|<password>|g" config/$environment.json
+sed -i "s|mysql-pass|password|g" config/$environment.json
 sed -i "s|env-db|"$environment"_allspark|g" config/$environment.json
 npm install -g typescript@next
 npm install --save underscore
@@ -27,7 +27,7 @@ NODE_ENV="$environment" pm2 start bin/www --name "$environment-comp"
 sleep 20
 
 #below port is for allspark
-curl http://localhost:<3001>/api/v2/setup/run
+curl http://localhost:3001/api/v2/setup/run
 pm2 restart all
 
 cd /apps/node-apps/allspark/
